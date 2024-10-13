6049375ed071beb4dd07bb6806dc0d69612ef61d
dmy.berezovskyi <dmy.berezovskyi@foodtech.team>
2024-10-13 23:53:15 +0300

    fix ruff

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ settings = Dynaconf(
     load_dotenv=True,  # Load envvars from a file named `.env`
     # TIP: probably you don't want to load dotenv on production environments
     #      pass `load_dotenv={"when": {"env": {"is_in": ["development"]}}}
-    envvar_prefix="PW",  # variables exported as `PW_FOO=bar` becomes `settings.FOO == "bar"`
+    envvar_prefix="PW",  # variables as `PW_FOO=bar` becomes `settings.FOO == "bar"`
     env_switcher="ENV_FOR_PW",  # to switch environments `export ENV_FOR_PW=production`
     dotenv_path="configs/.env",  # custom path for .env file to be loaded
 )
